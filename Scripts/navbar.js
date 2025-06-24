2364d4bbebe6808988dc8b782ebce0abf08f0eb3
Mostafa-El-Masry <mostafa.abdelfattah2021@gmail.com>
2025-06-24 07:07:19 +0300

    index.html clean up

--- Scripts/navbar.js
+++ Scripts/navbar.js
@@ -1,4 +1,13 @@
-
-  fetch('/Components/navbar.html')
+fetch('/Components/navbar.html')
     .then(res => res.text())
     .then(html => document.getElementById('navbar-placeholder').innerHTML = html);
+
+// nav bar toggle on mobile
+function toggleMobileMenu() {
+  const nav = document.getElementById('main-nav');
+  if (nav.classList.contains('hidden')) {
+    nav.classList.remove('hidden');
+  } else {
+    nav.classList.add('hidden');
+  }
+}
